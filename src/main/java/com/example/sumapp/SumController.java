f3ddccb3b040cea74f0d03e36554f00eeadb0942
Ronak Pradhan <pradhanronak@gmail.com>
2024-06-10 23:33:14 +0530

    Sumapp

--- src/main/java/com/example/sumapp/SumController.java
+++ src/main/java/com/example/sumapp/SumController.java
@@ -8,9 +8,29 @@ import org.springframework.web.bind.annotation.RestController;
 public class SumController {
 
     @GetMapping("/sum")
-    public String sum(@RequestParam int num1, @RequestParam int num2) {
-        int result = num1 + num2;
-        System.out.println("The sum of " + num1 + " and " + num2 + " is " + result);
-        return "The sum is " + result;
+    public String sum(
+            @RequestParam int num1,
+            @RequestParam int num2,
+            @RequestParam(required = false) String operation) {
+
+        int result;
+        switch (operation) {
+            case "subtract":
+                result = num1 - num2;
+                break;
+            case "multiply":
+                result = num1 * num2;
+                break;
+            case "divide":
+                result = num1 / num2;
+                break;
+            case "add":
+            default:
+                result = num1 + num2;
+                break;
+        }
+
+        System.out.println("The result of " + operation + " operation on " + num1 + " and " + num2 + " is " + result);
+        return "The result is " + result;
     }
 }
